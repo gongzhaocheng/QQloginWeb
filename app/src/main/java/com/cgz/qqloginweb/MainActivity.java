898a86de4939358f3490de2fd44bca98683bc27b
chenggongzhao <gongzhao.cheng@rayjoy.com>
2019-03-31 13:56:34 +0800

    找到，布局界面所有控件

--- app/src/main/java/com/cgz/qqloginweb/MainActivity.java
+++ app/src/main/java/com/cgz/qqloginweb/MainActivity.java
@@ -2,12 +2,29 @@ package com.cgz.qqloginweb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEtNumber;
+    private EditText mEtPassword;
+    private CheckBox mCbRemember;
+    private Button mBtLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEtNumber = findViewById(R.id.et_number);
+        mEtPassword = findViewById(R.id.et_password);
+        mCbRemember = findViewById(R.id.cb_remember);
+        mBtLogin = findViewById(R.id.bt_login);
+
+
+
     }
 }
